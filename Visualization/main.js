223a2a28d840c229b23dcf06ce082ad13bf47572
Evan Jelley <jelleyevan@gmail.com>
2024-02-16 14:27:12 -0700

    Starting work with Visualization

---
+++ Visualization/main.js
@@ -0,0 +1,21 @@
+import './style.css';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import {fromLonLat} from 'ol/proj.js';
+
+
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: fromLonLat([-96, 39.8283]),
+    zoom: 4.75
+  })
+});
